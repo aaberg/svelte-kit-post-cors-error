a4b9ac301fd8f92490ac043de8c01fcb4ac3161e
Martin Færøy <martin.faeroey@elaway.io>
2024-02-06 14:46:36 +0100

    updated packages, removed irrelevant packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script>
     export let form
-    
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <form method="post">
     <input type="text" name="name" placeholder="Your name" />
